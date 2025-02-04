2f32bf6350378a65a38530979b367cf451c4ec1e
Ronaldo Gandra Rocha <ronaldogandrarocha@gmail.com>
2025-02-03 21:34:51 -0300

    Finalizando Projeto

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
 # Consumo_API
 Consumo de API com Fetch em JavaScript.
+Código realizado pelos vídeos do YouTube do canal do Gustavo Caetano.
+Projeto prático realizado com JavaScript puro a fim de estudar conceitos como Promises, Async/Await, HOF(High Order Functions), filtragem e manipulação de dados de uma API, validação de formulário, assim como a criação de elementos HTML(DOM) e propriedades CSS dinâmicamente.
 
-
-
+## Link da playlist das aulas e do canal do Gustavo
+* https://youtu.be/kbuLUcloSfY?si=aESLC7RKaRWIo0xQ
+* https://www.youtube.com/@oguscaetano
 
 ## Observações de código: 
 Método JSON.stringify - parâmetros
 O método JSON.stringify(data, undefined, 2) melhora a formatação do JSON para torná-lo mais legível no HTML. O parâmetro undefined mantém os dados sem alterações, e o 2 define a indentação com dois espaços, organizando o JSON com quebras de linha e recuos, facilitando a leitura.
+
+ * imagem.setAttribute("src", data.image), tem a mesma função que imagem.src = `${data.image}`;
+
+* .join("\r\n") = pega todos os elementos do array  e junta-os em uma única string, separando cada elemento por uma quebra de linha no formato Windows (\r\n). No caso, substitui "\r\n" para "<br>", assim, a quebra de linha é exibida no HTML.
+
+* Para a validação dos checkbox criados dinâmicamente dentro da função buildResult(), foi necessário criar uma nova variável dentro do addEventListener selecionando todos elementos que possuem o ID igual ao elemento do array 'keys' e filtrando cada um deles quando forem selecionados, no final ao utilizar o método '.length' conseguimos perceber quantos elementos foram selecionados. Se o resultado for 0, quer dizer que nenhum foi selecionado, logo é exibido uma mensagem para o usuário selecionar pelo menos um campo do checkbox. Abaixo a linha de código que é responsável por isso: <br>
+```
+const checkedElements = keys.map((key) => document.getElementById(key))                         .filter((element) => element.checked === true);
+
+if (checkedElements.length === 0) {
+    window.alert("Selecione pelo menos um checkbox antes de buscar.");
+    return;}```
--- app.js
+++ app.js
@@ -1,40 +1,85 @@
-
 const characterId = document.querySelector('input#characterId');
 const button = document.querySelector('button#btn-go');
 const limpar = document.querySelector('button#clear');
-const content = document.querySelector('pre#content');
+const content = document.querySelector('div#content');
 const imagem = document.querySelector('img#img');
-
+const containerResult = document.querySelector('div#result-style');
 
 
 const  fetchApi = async (id) => {
   try {
     const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
-    const data = await response.json();
-    console.log("PERSONAGEM: ", data);
-    content.innerHTML = '';
-    imagem.setAttribute("src", data.image);
-    return  JSON.stringify(data, undefined, 2);
-    // const { name, species, status, gender } = data;
-    // return content.innerHTML += `<p> Nome: ${name}\n Espécie: ${species}\n Status: ${status}\n Sexo: ${gender}</p>`
+    const data = await response.json();  
+    return data;
   } catch (error) {
     console.error("ERROR: ", error);
   }
 };
 
 
+const keys = ['name', 'status','species' ,'gender','origin', 'location', 'episode'];
+
+const newKeys = {
+    name: 'Nome',
+    status: 'Status',
+    species: 'Espécie',
+    gender: 'Gênero',
+    origin: 'Planeta de origem',
+    location: 'Localização atual',
+    episode: 'Episódio(s)',
+}
+
+
+const buildResult = (result) => {
+   content.innerHTML = '';
+       return  keys.map((key) => document.getElementById(key))
+            .map((element) => {
+                if (element.checked === true && (Array.isArray(result[element.name]) === true )){
+                    const arrayResult = result[element.name].join('<br>');
+                    const newElement = document.createElement('p');
+                    newElement.innerHTML = `<strong>${newKeys[element.name]}</strong>: <br>${arrayResult}`;
+                    content.appendChild(newElement);
+                }
+                else if (element.checked === true && typeof(result[element.name]) === 'object'){
+                    const newElement = document.createElement('p');
+                    newElement.innerHTML = `<strong>${newKeys[element.name]}</strong>: ${result[element.name].name}`;
+                    content.appendChild(newElement);
+                }
+                
+                else if (element.checked === true && typeof(result[element.name]) !== 'object'){
+                    const newElement = document.createElement('p');
+                    newElement.innerHTML = `<strong>${newKeys[element.name]}</strong>: ${result[element.name]}`;
+                    content.appendChild(newElement);
+                }
+             })   
+};
 
 button.addEventListener('click',  async (event) => {
   event.preventDefault();
-  const result =  await fetchApi(characterId.value);
-  return content.textContent = `${result}`;
-  
+
+  if (characterId.value === '' || characterId.value <= 0 || characterId.value != parseInt(characterId.value)) {
+    window.alert("Preencha o ID da personagem para continuar. \nSomente valores inteiros e positivos");
+    return;
+  } 
+
+    const checkedElements = keys.map((key) => document.getElementById(key))
+                            .filter((element) => element.checked === true);
+
+    if (checkedElements.length === 0) {
+    window.alert("Selecione pelo menos um checkbox antes de buscar.");
+    return;
+    }
+
+    const result =  await fetchApi(characterId.value);
+    buildResult(result);
+    containerResult.className = 'result-style';
+    imagem.src = `${result.image}`; 
 });
 
-limpar.addEventListener('click', (event) => {
-  event.preventDefault();
-  characterId.value = '';
+limpar.addEventListener('click', () => {
+  // location.reload()   -> limpa, mas atualiza a página inteira;
   imagem.setAttribute('src', '');
+  containerResult.className = '';
   characterId.focus();
   return content.innerHTML = '';
 })
